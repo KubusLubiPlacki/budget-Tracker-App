b2c45558965543202910b108b19c5c1cc552b994
Jakub <kubarafalski240703@gmail.com>
2025-07-25 22:32:42 +0200

    few changes in css, added chart js(repair it)

--- script.js
+++ script.js
@@ -2,13 +2,52 @@ const nameTransaction=document.getElementById("name-transaction");
 const amount=document.getElementById("amount");
 const btn=document.querySelector("button");
 
+const ctx = document.getElementById("myChart");
+let xValues=["Income","Expense"];
+let yValues=[100,200];
+let barColors=["#3F9597","#FA8C71"];
+
 let price=0
+let expenses=0;
+let balance=0;
 
 function updateData(number){
-    price+=Number(number);
-    document.querySelector(".balance").textContent=`$${price}`;
-    document.querySelector(".income").textContent=`$${price}`;
-    // repair income (it shouldnt remove money from it)
+    if(number>0){
+        price+=Number(number);
+        document.querySelector(".income").textContent=`$${price}`;
+        yValues[0]=yValues+price;
+    }
+    if(number<0){
+        expenses+=Number(number);
+        document.querySelector(".expenses").textContent=`$${expenses}`;
+        if(expenses<0){
+           document.querySelector(".expenses").style.color="#FA8C71" 
+        }
+        yValues[0]=yValues+expenses;
+    }
+    let balance=price+expenses;
+    if(balance>=0){
+        document.querySelector(".balance").style.color="#3F9597";
+    }
+    else{
+        document.querySelector(".balance").style.color="#FA8C71";
+    }
+    document.querySelector(".balance").textContent=`$${balance}`;
+    new Chart(ctx, {
+    type: "bar",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+    },
+    options: {
+        legend: {display: false},
+    }
+    });
+    //repair graph
+    
 }
 
 function createExpense(name,price){
@@ -19,7 +58,9 @@ function createExpense(name,price){
     let priceExpense=document.createElement("p");
     if(price[0]=="-"){
         priceExpense.classList.add("element-price-minus");
-        priceExpense.innerHTML=`$${price}`;
+        let arr=[...price];
+        arr.splice(1,0,"$");
+        priceExpense.innerHTML=`${arr.join("")}`;
     }
     else{
         priceExpense.classList.add("element-price-plus");
