35341a07363d879addbfd7f164aba8293f31b7f2
Jakub <kubarafalski240703@gmail.com>
2025-07-27 15:26:31 +0200

    repaired chart.js

--- script.js
+++ script.js
@@ -4,36 +4,16 @@ const btn=document.querySelector("button");
 
 const ctx = document.getElementById("myChart");
 let xValues=["Income","Expense"];
-let yValues=[100,200];
+let yValues=[0,0];
 let barColors=["#3F9597","#FA8C71"];
+let newData=[0,0];
+
 
 let price=0
 let expenses=0;
 let balance=0;
 
-function updateData(number){
-    if(number>0){
-        price+=Number(number);
-        document.querySelector(".income").textContent=`$${price}`;
-        yValues[0]=yValues+price;
-    }
-    if(number<0){
-        expenses+=Number(number);
-        document.querySelector(".expenses").textContent=`$${expenses}`;
-        if(expenses<0){
-           document.querySelector(".expenses").style.color="#FA8C71" 
-        }
-        yValues[0]=yValues+expenses;
-    }
-    let balance=price+expenses;
-    if(balance>=0){
-        document.querySelector(".balance").style.color="#3F9597";
-    }
-    else{
-        document.querySelector(".balance").style.color="#FA8C71";
-    }
-    document.querySelector(".balance").textContent=`$${balance}`;
-    new Chart(ctx, {
+const myChart=new Chart(ctx, {
     type: "bar",
     data: {
         labels: xValues,
@@ -43,11 +23,39 @@ function updateData(number){
         }]
     },
     options: {
-        legend: {display: false},
+        legend: {display: true},
     }
     });
-    //repair graph
+
+
+
+function updateData(number){
+    if(number>0){
+        price+=Number(number);
+        document.querySelector(".income").textContent=`$${price}`;
+        newData[0]=newData[0]+price;
+        myChart.data.datasets[0].data[0]=newData[0];
+
+    }
+    if(number<0){
+        expenses+=Number(number);
+        document.querySelector(".expenses").textContent=`$${expenses}`;
+        if(expenses<0){
+           document.querySelector(".expenses").style.color="#FA8C71" 
+        }
+        newData[1]=newData[1]+expenses*(-1);
+        myChart.data.datasets[0].data[1]=newData[1];
+    }
+    let balance=price+expenses;
+    if(balance>=0){
+        document.querySelector(".balance").style.color="#3F9597";
+    }
+    else{
+        document.querySelector(".balance").style.color="#FA8C71";
+    }
+    document.querySelector(".balance").textContent=`$${balance}`;
     
+    myChart.update()
 }
 
 function createExpense(name,price){
@@ -66,8 +74,6 @@ function createExpense(name,price){
         priceExpense.classList.add("element-price-plus");
         priceExpense.innerHTML=`+$${price}`;
     } 
-    // repair sequence of characters(should be for example -$1200, now its $-1200)
-    
     nameExpense.innerText=name;
 
     nameExpense.classList.add("element-name");
